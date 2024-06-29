c14bb35e27f915364817bc8b44ba057a1c4b84d8
Snwqqq <stanislav.cards@gmail.com>
2024-06-29 13:20:06 +0300

    Correct pseudocode, create const output and input windows

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-ask user to pick rock,paper or scissors
-give numeric const to rock,paper, scissorc
-convert it into numeric value
-random a value from the computer 1to3
-create switch and based on user input check who win or tie
-display result to the user
-increase score if not a tie
-start over until score is 3
+//create value witch will store user input + add value for win
+let userChoice;
+let userWin=false;
+//ask user to pick rock,paper or scissors
+userChoice=window.prompt("Pick:\n1.Rock\n2.Paper\n3.Scissors",'');
+//give numeric const to rock,paper, scissorc
+const ROCK = 1;
+const PAPER = 2;
+const SCISSORS = 3;
+//convert it into numeric value if it is a string
+if(typeof userChoice != Number){
+    userChoice = userChoice.toUpperCase();
+    alert(userChoice);
+    switch(userChoice){
+    case "ROCK": 
+    
+    break;
+    }
+}
+//random a value from the computer 1to3
+//create switch and based on user input check who win or tie
+//display result to the user
+//increase score if not a tie
+//start over until score is 3
