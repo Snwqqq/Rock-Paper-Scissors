d2cc88cda82844b356b55af3b7b799cc64c6e862
Snwqqq <stanislav.cards@gmail.com>
2024-06-29 13:03:02 +0300

    Create index.html add basic structure, cr. script.js add pseudo code

---
+++ script.js
@@ -0,0 +1,8 @@
+ask user to pick rock,paper or scissors
+give numeric const to rock,paper, scissorc
+convert it into numeric value
+random a value from the computer 1to3
+create switch and based on user input check who win or tie
+display result to the user
+increase score if not a tie
+start over until score is 3
